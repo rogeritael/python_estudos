8720a5c391b18988b961555451953da653630477
Roger Rosa <rogeritael.contato@gmail.com>
2023-10-23 00:44:00 -0300

    criando mais funçoes para interagir com o sistema operacional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import platform
+import shutil
 
 class OperatingSystem:
     def __init__(self):
@@ -51,12 +52,40 @@ class OperatingSystem:
         #cria multiplas pastas
         os.makedirs(f'{folder_name}/{folder_name}/{folder_name}')
 
+    # renomear arquivos
+    def renameFile(self, filePath: str, newName: str):
+        os.rename(filePath, newName)
+        # renameFile('dir_files', 'arquivos')
 
-    def isFolderAlreadyCreated(self):
-        pass
+    # os.rename tambem move arquivos
+    def moveFile(self, currentPath: str, newPath: str):
+        os.rename(currentPath, newPath)
+        # moveFile('arquivos/abc.py', 'arquivos/moved/abd.py')
 
+    def copyFile(self, file: str, copyTo: str):
+        shutil.copy2(file, copyTo)
+        # copyFile('arquivos/file.py', 'arquivos/moved/')
+        # copyFile('arquivos/file.py', 'arquivos/moved/movedfile.txt') copiando e renomeando
+
+    # def copyEntireFolder():
+    #     copy_tree('arquivos/moved', 'arquivos')
+
+    #deletando pastas e arquivos
+    def deleteFile(self, filePath: str, fileType: str):
+        # para deletar arquivos
+        if fileType == 'folder':
+            # para pastas com arquivos
+            if len(os.listdir(filePath)) > 0:
+                shutil.rmtree(filePath)
+            # para pastas vazias
+            else:
+                os.removedirs(filePath)
+        elif fileType == 'file':
+            os.remove(filePath)
+        else:
+            print('tipo do arquivo nao definido corretamente ou arquivo nao existe')
 
 
 
 system = OperatingSystem()
-system.createFolder('newfolder')
+system.deleteFile('arquivos/newfolder', 'folder')
